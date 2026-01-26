93c54d05c48927ca718996de78dff38049dd89ec
Arnav-Bhardwaj1 <arnavbhardwaj111@gmail.com>
2026-01-27 02:15:50 +0530

    worked on the Editor Page

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
-    <input
+    <input // returns the <input> HTML element, which has inbuilt props like type, value, onChange, etc.
       type={type}
       data-slot="input"
       className={cn(
