e9f5a36081951f3b9f901f9538bcd7a5ba2ae0b7
Arnav-Bhardwaj1 <arnavbhardwaj111@gmail.com>
2026-02-18 18:27:09 +0530

    webhooks issue fix

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,20 +1,13 @@
+import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const dynamic = 'force-dynamic';
 export const runtime = 'nodejs';
 
-let handler: ReturnType<typeof toNextJsHandler> | null = null;
+const handler = toNextJsHandler(auth);
 
-const getHandler = async () => {
-  if (!handler) {
-    const { auth } = await import("@/lib/auth");
-    handler = toNextJsHandler(auth);
-  }
-  return handler;
-};
-
-export const GET = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["GET"]>) => (await getHandler()).GET(...args);
-export const POST = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["POST"]>) => (await getHandler()).POST(...args);
-export const PUT = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["PUT"]>) => (await getHandler()).PUT(...args);
-export const PATCH = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["PATCH"]>) => (await getHandler()).PATCH(...args);
-export const DELETE = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["DELETE"]>) => (await getHandler()).DELETE(...args);
+export const GET = handler.GET;
+export const POST = handler.POST;
+export const PUT = handler.PUT;
+export const PATCH = handler.PATCH;
+export const DELETE = handler.DELETE;
